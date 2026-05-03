d775cb19efed29949c077f6c21f3eb592596a7df
PDgaming <produnyadehingia12345@gmail.com>
2026-05-03 19:40:48 +0530

    types: added the data structure types

---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+type status = 'todo' | 'in-progress' | 'done';
+
+export interface card {
+	id: string; // Use UUIDs
+	title: string;
+	description: string;
+	status: status;
+}
+
+export interface column {
+	id: string; // Use UUIDs
+	title: string;
+	status: status;
+	cards: card['id'][];
+}
